dbd150dae77e413fca8f2fd99285cfb45549e4c3
AlexandrHil <>
2021-04-04 14:21:18 +0300

    added swiftlint

--- FinalProject/FinalProject/ViewController.swift
+++ FinalProject/FinalProject/ViewController.swift
@@ -11,9 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = UIColor(red: 253/255, green: 240/255, blue: 213/255, alpha: 1.0)
+
     }
-
-
 }
-
